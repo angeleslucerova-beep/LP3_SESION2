413e2f5cd6c4e73b468ad4b85984bfa9fd13a52c
angeleslucerova-beep <angeleslucerova@gmail.com>
2026-08-27 12:46:35 -0500

    Create Objeto.java

---
+++ Ejercicios propuestos/EjercicioPropuesto2/Objeto.java
@@ -0,0 +1,11 @@
+public class Objeto {
+    private String nombreObjeto;
+
+    public Objeto(String nombreObjeto) {
+        this.nombreObjeto = nombreObjeto;
+    }
+
+    public String getNombreObjeto() {
+        return nombreObjeto;
+    }
+}
